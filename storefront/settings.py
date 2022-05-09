e4b99c69a1e7b5ab83428b8f51821c9545d216b6
Sergio Márquez <smarquezs@gmail.com>
2022-05-09 10:24:05 -0400

    Add PostgreSQL to project.

--- storefront/settings.py
+++ storefront/settings.py
@@ -83,8 +83,12 @@ WSGI_APPLICATION = 'storefront.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'store_development',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
