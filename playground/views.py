6229dbc08d36e6f232eb76b33f13afbe1edc40df
Sergio Márquez <smarquezs@gmail.com>
2022-05-09 10:24:52 -0400

    Play with ORM.

--- playground/views.py
+++ playground/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from store.models import Customer, Product
 
 def hello_world(request):
-    return render(request, 'hello.html')
+    products = Product.objects.order_by('-title')[:5]
+
+    return render(request, 'hello.html', {'products': list(products)})
